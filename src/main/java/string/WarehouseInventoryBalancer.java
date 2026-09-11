a66c89b90b8609880104dcdb767c7eefa486b973
luth-fa <luth5572gmail.com>
2026-09-11 19:01:13 +0530

    Add warehouse inventory balancer assignment

---
+++ src/main/java/string/WarehouseInventoryBalancer.java
@@ -0,0 +1,79 @@
+package string;
+
+import java.util.Scanner;
+
+public class WarehouseInventoryBalancer {
+
+    static void analyzeInventory(int[] sectionA, int[] sectionB) {
+
+        int totalA = 0;
+        int totalB = 0;
+
+        for (int i = 0; i < sectionA.length; i++) {
+            totalA += sectionA[i];
+        }
+
+        for (int i = 0; i < sectionB.length; i++) {
+            totalB += sectionB[i];
+        }
+
+        System.out.println("Section A Total: " + totalA);
+        System.out.println("Section B Total: " + totalB);
+
+        if (totalA == totalB) {
+            System.out.println("Balanced");
+        } else {
+            System.out.println("Not Balanced");
+        }
+
+        int highest = sectionA[0];
+        String section = "A";
+        int index = 0;
+
+        for (int i = 1; i < sectionA.length; i++) {
+            if (sectionA[i] > highest) {
+                highest = sectionA[i];
+                section = "A";
+                index = i;
+            }
+        }
+
+        for (int i = 0; i < sectionB.length; i++) {
+            if (sectionB[i] > highest) {
+                highest = sectionB[i];
+                section = "B";
+                index = i;
+            }
+        }
+
+        System.out.println("Highest Quantity: " + highest);
+        System.out.println("Section " + section + ", Item " + (index + 1));
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of items in each section: ");
+        int n = sc.nextInt();
+
+        int[] sectionA = new int[n];
+        int[] sectionB = new int[n];
+
+        System.out.println("Enter quantities for Section A:");
+
+        for (int i = 0; i < n; i++) {
+            sectionA[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter quantities for Section B:");
+
+        for (int i = 0; i < n; i++) {
+            sectionB[i] = sc.nextInt();
+        }
+
+        analyzeInventory(sectionA, sectionB);
+
+        sc.close();
+    }
+}
